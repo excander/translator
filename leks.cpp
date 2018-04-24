199b480e53014401ecd6f14daf5e967bd6d2e8f4
Artem Miklin <excander@gmail.com>
2018-04-24 21:50:49 +0300

    Added class Scanner (without get_lex() method)

--- leks.cpp
+++ leks.cpp
@@ -99,6 +99,80 @@ int Tabl_ident::put(char *buf){
 	return top-1;
 }
 
+////////////////////////////////////////////////////////////////////
+
+Tabl_ident TID(100);
+
+/////////////////////  Класс Scanner  //////////////////////////////
+
+class Scanner {
+public:
+			enum state { H, IDENT, NUMB, STRN, ALE, NEQ, COMDEL, DELIM };
+	static char* TW[];
+	static type_of_lex words[];
+	static char* TD[];
+	static type_of_lex dlms[];
+			state CS;
+			FILE *fp;
+			char c;
+			char buf[80];
+			int buf_top;
+			void clear(){
+				buf_top = 0;
+				for(int i=0; i<80; i++)
+					buf[i] = '\0';
+			}
+			void add(){
+				buf[buf_top++] = c;
+			}
+
+			int look(const char *buf, char **list){
+				int i = 0;
+				while (list[i]){
+					if ( !strcmp(buf, list[i]) )
+						return i;
+					i++;
+				}
+				return 0;
+			}
+			void gc(){ c = fgetc(fp); }
+// public:
+		Scanner (const char *program){
+			fp = fopen(program, "r");
+			CS = H;
+			clear();
+			gc();
+		}
+		Lex get_lex();
+};
+
+char * 
+Scanner::TW[] {"", "program",  "int", "string", "if", "else", "while", "read", "write", "not", "and", "or", "break", "case", "of", "end", "do", NULL};
+
+type_of_lex
+Scanner::words[] {
+	LEX_NULL, 								// 0
+	LEX_PROGRAM, LEX_INT, LEX_STRING, 		// 1 2 3
+	LEX_IF, LEX_ELSE, LEX_WHILE, 			// 4 5 6 
+	LEX_READ, LEX_WRITE, LEX_NOT, 			// 7 8 9
+	LEX_AND, LEX_OR, LEX_BREAK, 			// 10 11 12
+	LEX_CASE, LEX_OF, LEX_END, LEX_DO,		// 13 14 15
+};
+
+char *
+Scanner::TD[] { "", "{", "}", ",", "=", "+", "-", "\"", "*", "/", "<", ">", "<=", ">=", "==", "!=", "(", ")", ":", "%", NULL};
+type_of_lex
+Scanner::dlms[] {
+	LEX_NULL,
+	LEX_LBRACE, LEX_RBRACE, LEX_SEMICOLON, 	// { } ;
+	LEX_COMMA, LEX_ASSIGN, LEX_PLUS,		// , = +
+	LEX_MINUS, LEX_QUOTE, LEX_TIMES, 		// - " *			
+	LEX_SLASH, LEX_LSS, LEX_GTR,			// / < >
+	LEX_LEQ, LEX_GEQ, LEX_EQ,				// <= >= ==
+	LEX_NEQ, LEX_LPAREN, RPAREN,			// != (  )
+	LEX_COLON, LEX_PERCENT					// : %
+};
+
 
 
 
@@ -106,5 +180,11 @@ int main() {
 	Lex l;
 	Ident id1;
 	id1.put_strval("asd");
-	cout << id1.get_strval();
+	cout << id1.get_strval() << endl;
+	Scanner scan1("input.txt");
+	int i = 1;
+	while(scan1.TD[i]){
+		cout << scan1.TD[i]<< endl;
+		i++;
+	}
 }
