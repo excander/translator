e1af8e1940e30105714ab564a6153bc7e98e76ae
Artem Miklin <excander@gmail.com>
2018-05-14 16:33:55 +0300

    Скопирован класс Executer и его вызов.

--- leks.cpp
+++ leks.cpp
@@ -536,7 +536,7 @@ public:
 void Parser::analyze(){
 	gl();
 	Programma();
-	cout << endl << "Success!!!" << endl;
+	// cout << endl << "Success!!!" << endl;
 }
 
 void Parser::Programma(){
@@ -888,7 +888,7 @@ void Parser::check_not ()
   }
 }
 
-////////////////проверка типов в операторах////////////////////////
+////////////////проверка типов в операторах////////////////////
 
 void Parser::eq_type (){
     if (st_lex.pop() != st_lex.pop())
@@ -905,13 +905,161 @@ void Parser::check_id_in_read (){
         string("Error: Variable ") + string(TID[c_val].get_name()) + string(" is not declared in read operator!");
 }
 
-int main() {
-    // Lex l;
-    // Ident id1;
-    // id1.put_strval("asd");
-    // cout << id1.get_strval() << endl; 
+////////////////////////////////////////////////////////////////
  
+class Executer
+{
+         Lex          pc_el;
+public:
+         void         execute ( Poliz & prog );
+};
+ 
+void Executer::execute ( Poliz & prog )
+{
+  Stack < int, 100 > args;
+  int i, j, index = 0, size = prog.get_free();
+  while ( index < size )
+  {
+    pc_el = prog [ index ];
+    switch ( pc_el.get_type () )
+    {
+      case LEX_NUM: case POLIZ_ADDRESS: case POLIZ_LABEL:
+        args.push ( pc_el.get_value () );
+        break;
+      // case LEX_ID:
+      //   i = pc_el.get_value ();
+      //   if ( TID[i].get_assign () )
+      //   {
+      //     args.push ( TID[i].get_value () );
+      //     break;
+      //   }
+      //   else
+      //     throw "POLIZ: indefinite identifier";
+      case LEX_NOT:
+        args.push( !args.pop() );
+        break;
+      case LEX_OR:
+        i = args.pop();
+        args.push ( args.pop() || i );
+        break;
+      case LEX_AND:
+        i = args.pop();
+        args.push ( args.pop() && i );
+        break;
+      case POLIZ_GO:
+        index = args.pop() - 1;
+        break;
+      case POLIZ_FGO:
+        i = args.pop();
+        if ( !args.pop() ) index = i-1;
+        break;
+      case LEX_WRITE:
+        cout << args.pop () << endl;
+        break;
+      // case LEX_READ:
+      //   {
+      //     int k;
+      //     i = args.pop ();
+      //     if ( TID[i].get_type () == LEX_INT )
+      //     {
+      //       cout << "Input int value for" << TID[i].get_name () << endl;
+      //       cin >> k;
+      //     }
+      //     else
+      //     {
+      //       char j[20];
+      //       rep:
+      //       cout << "Input boolean value (true or false) for" << TID[i].get_name() << endl;
+      //       cin >> j;
+      //       if (!strcmp(j, "true"))
+      //         k = 1;
+      //       else
+      //         if (!strcmp(j, "false"))
+      //           k = 0;
+      //         else
+      //         {
+      //           cout << "Error in input:true/false" << endl;
+      //           goto rep;
+      //         }
+      //     }
+      //     TID[i].put_value (k);
+      //     TID[i].put_assign ();
+      //     break;
+      //   }
+      case LEX_PLUS:
+        args.push ( args.pop() + args.pop() );
+        break;
+      case LEX_TIMES:
+        args.push ( args.pop() * args.pop() );
+        break;
+      case LEX_MINUS:
+        i = args.pop();
+        args.push ( args.pop() - i );
+        break;
+      case LEX_SLASH:
+        i = args.pop();
+        if (!i)
+        {
+          args.push(args.pop() / i);
+          break;
+        }
+        else
+          throw "POLIZ:divide by zero";
+      case LEX_EQ:
+        args.push ( args.pop() == args.pop() );
+        break;
+      case LEX_LSS:
+        i = args.pop();
+        args.push ( args.pop() < i);
+        break;
+      case LEX_GTR:
+        i = args.pop();
+        args.push ( args.pop() > i );
+        break;
+      case LEX_LEQ:
+        i = args.pop();
+        args.push ( args.pop() <= i );
+        break;
+      case LEX_GEQ:
+        i = args.pop();
+        args.push ( args.pop() >= i );
+        break;
+      case LEX_NEQ:
+        i = args.pop();
+        args.push ( args.pop() != i );
+        break;
+      // case LEX_ASSIGN:
+      //   i = args.pop();
+      //   j = args.pop();
+      //   TID[j].put_value(i);
+      //   TID[j].put_assign(); break;
+      default:
+        throw "POLIZ: unexpected elem";
+    }//end of switch
+    ++index;
+  };//end of while
+  cout << "Finish of executing!!!" << endl;
+}
+ 
+class Interpretator
+{
+  Parser   pars;
+  Executer E;
+public:
+           Interpretator  (const char* program): pars (program) {}
+  void     interpretation ();
+};
+ 
+void Interpretator::interpretation ()
+{
+  pars.analyze ();
+  cout << endl;
+  pars.prog.print();
+  cout << endl;
+  E.execute ( pars.prog );
+}
 
+int main() {
 	// Scanner scan1("input.txt");
  //    try {
 	//     Lex lex1;
@@ -927,11 +1075,23 @@ int main() {
  //    	cout << str << endl;
 	// }
 
-	Parser pars1("input.txt");
-	try {
-		pars1.analyze();
-        pars1.prog.print();
-	}
+// // Проверка работы лексич+семантич анализа и генерации полиза
+// 	Parser pars1("input.txt");
+// 	try {
+// 		pars1.analyze();
+//         pars1.prog.print();
+// 	}
+  try
+  {
+    Interpretator I ("input.txt");
+    I.interpretation ();
+    return 0;
+  }
+
+  catch (char c){
+    cout << "unexpected symbol " << c << endl;
+    return 1;
+  }
 	catch (char const* str){
     	cout << str << endl;
     }
