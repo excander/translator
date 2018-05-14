5146435a96901fc4503aa2c9ea55c2dd9eeef5c9
Artem Miklin <excander@gmail.com>
2018-05-14 12:44:31 +0300

    В файле example.cpp добавлен массив строковых представлений лексем string str_lex[100] = {"LEX_NUM", "LEX_PROGRAM", ... } Для читабельного вывода лексем.

--- example/example.cpp
+++ example/example.cpp
@@ -20,6 +20,20 @@ enum type_of_lex
   POLIZ_ADDRESS, /*39*/
   POLIZ_GO, /*40*/
   POLIZ_FGO}; /*41*/
+   string str_lex[100] =
+{
+  "LEX_NULL", /*0*/
+  "LEX_AND", "LEX_BEGIN", "LEX_BOOL", "LEX_DO", "LEX_ELSE", "LEX_END", "LEX_IF", "LEX_FALSE", "LEX_INT", /*9*/
+  "LEX_NOT", "LEX_OR", "LEX_PROGRAM", "LEX_READ", "LEX_THEN", "LEX_TRUE", "LEX_VAR", "LEX_WHILE", "LEX_WRITE", /*18*/
+  "LEX_FIN", /*19*/
+  "LEX_SEMICOLON", "LEX_COMMA", "LEX_COLON", "LEX_ASSIGN", "LEX_LPAREN", "LEX_RPAREN", "LEX_EQ", "LEX_LSS", /*27*/
+  "LEX_GTR", "LEX_PLUS", "LEX_MINUS", "LEX_TIMES", "LEX_SLASH", "LEX_LEQ", "LEX_NEQ", "LEX_GEQ", /*35*/
+  "LEX_NUM", /*36*/
+  "LEX_ID", /*37*/
+  "POLIZ_LABEL", /*38*/
+  "POLIZ_ADDRESS", /*39*/
+  "POLIZ_GO", /*40*/
+  "POLIZ_FGO"}; /*41*/
  
 /////////////////////////  Класс Lex  //////////////////////////
  
@@ -162,7 +176,7 @@ public:
          void         print ()
                       {
                         for ( int i = 0; i < free; ++i )
-                          cout << p[i];
+                          cout << str_lex[p[i].get_type()] << endl;
                       }
 };
  
